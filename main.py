a55100d3f42f01268c4238e94063f3619847afea
Danar Putera <danar.g.putera@gmail.com>
2025-09-18 10:51:02 +0700

    Finish: Debugging

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import logging
+
 from omegaconf import DictConfig, OmegaConf
 import hydra
 
+logger = logging.getLogger(__name__)
 
-@hydra.main(config_path='.', config_name='config')
+
+@hydra.main(version_base=None, config_path='configs', config_name='config')
 def main(config: DictConfig) -> None:
     print(OmegaConf.to_yaml(config))
+    logger.info('Default info message')
+    logger.debug('Default debug message')
 
 
 if __name__ == "__main__":
